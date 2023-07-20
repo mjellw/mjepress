64bdcc591c11ae6947ff1ac0546efabc974e92ad
tentoumushi-jp <09mjellw@gmail.com>
2023-07-20 22:40:55 +0900

    いろいろ書いた

--- src/.vuepress/.temp/pages/index.html.vue
+++ src/.vuepress/.temp/pages/index.html.vue
@@ -1,4 +1,23 @@
-<template><div><h1 id="hello-vuepress" tabindex="-1"><a class="header-anchor" href="#hello-vuepress" aria-hidden="true">#</a> Hello VuePress</h1>
+<template><div><h1 id="hello-vuepress" tabindex="-1"><a class="header-anchor" href="#hello-vuepress" aria-hidden="true">#</a> Hello Vuepress!</h1>
+<p>はじめての VuePressを使ったドキュメント・ブログ作成</p>
+<h2 id="vuepressとは" tabindex="-1"><a class="header-anchor" href="#vuepressとは" aria-hidden="true">#</a> Vuepressとは？</h2>
+<ul>
+<li>なんかいい感じにドキュメントとかブログとか作れるやつ</li>
+<li>Vueが使える（たぶん）</li>
+</ul>
+<h2 id="なんでvuepress始めたんですか" tabindex="-1"><a class="header-anchor" href="#なんでvuepress始めたんですか" aria-hidden="true">#</a> なんでVuepress始めたんですか？</h2>
+<ul>
+<li>ブログ作れそうな気がしたから</li>
+<li>使いやすそうだったから</li>
+<li><strong>Misskey-hubで使われていたから</strong>←これがメイン</li>
+</ul>
+<h2 id="todo" tabindex="-1"><a class="header-anchor" href="#todo" aria-hidden="true">#</a> TODO</h2>
+<ul>
+<li>UIのカスタマイズ</li>
+<li>GitHub Pagesでデプロイ</li>
+<li>独自ドメイン化</li>
+<li>ブログ的運用</li>
+</ul>
 </div></template>
 
 
--- src/README.md
+++ src/README.md
@@ -1,1 +1,24 @@
-# Hello VuePress
+---
+title: Hello Vuepress!
+meta:
+  - name: description
+    content: すごい説明
+    sidebar: auto
+---
+# Hello Vuepress!
+はじめての VuePressを使ったドキュメント・ブログ作成
+## Vuepressとは？
+- なんかいい感じにドキュメントとかブログとか作れるやつ
+- Vueが使える（たぶん）
+
+## なんでVuepress始めたんですか？
+- ブログ作れそうな気がしたから
+- 使いやすそうだったから
+- **Misskey-hubで使われていたから**←これがメイン
+
+## TODO
+- UIのカスタマイズ
+- GitHub Pagesでデプロイ
+- 独自ドメイン化
+- ブログ的運用
+
